a1c470c2b1c83f5d09beefd9b425fd7d9ca8b160
Mehdi M <hi@mehdi.cc>
2023-08-14 22:54:09 +0200

    Slightly improve tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,36 +2,47 @@ import { beforeEach, describe, expect, test } from 'vitest'
 
 import CacheMap from '../src'
 
-const cache = new CacheMap()
+const cache = new CacheMap([[3, 62]])
 
-beforeEach(() => {
-  // clear the cache
-  cache.clear()
+beforeEach(() => cache.clear()) // empty the cache
 
-  cache.add('key', 1)
-  cache.add('key', 2)
-  cache.remember('memories', () => cache.get('key') + 14)
-  cache.remember('memories', () => cache.get('key') + 2)
-  cache.remember('memories primitive', 4)
-  cache.remember('memories primitive', 6)
-})
+describe('CacheMap extends Map', () => {
+  test('a CacheMap object is an instance of Map', () => expect(cache).toBeInstanceOf(Map))
 
-describe('The CacheMap class', () => {
-  test('extends Map', () => expect(cache).toBeInstanceOf(Map))
+  test('CacheMap and Map share the .clear method', () =>
+    expect(CacheMap.prototype.clear == Map.prototype.clear).toBeTruthy()
+  )
 })
 
 describe('CacheMap.add', () => {
   test('returns its CacheMap instance', () => expect(cache.add('key', 0)).toBe(cache))
-  test('uses the cache with direct value', () => expect(cache.get('key')).toBe(1))
+
+  test('caches a primitive value', () => {
+    cache.add('key', 1)
+    cache.add('key', 2)
+
+    expect(cache.get('key')).toBe(1)
+  })
 })
 
 describe('CacheMap.remember', () => {
-  test('uses the cache with computed value', () => expect(cache.get('memories')).toBe(15))
-  test('uses the cache with primitive non-computed value', () => expect(cache.get('memories primitive')).toBe(4))
+  test('caches the result of a sync handler', () => {
+    cache.remember('memories', () => 7 + 14)
+    cache.remember('memories', () => 3 + 2)
+
+    expect(cache.get('memories')).toBe(21)
+  })
+
+  test('caches a primitive value', () => {
+    cache.remember('memories primitive', 4)
+    cache.remember('memories primitive', 6)
+
+    expect(cache.get('memories primitive')).toBe(4)
+  })
 })
 
 describe('CacheMap.rememberAsync', () => {
-  test('async remember with async handler', async () => {
+  test('caches the result of an async handler', async () => {
 
     // function emulating an async network call
     const fetchMetadata = async () => ({ date: new Date(), things: ['thing1'] })
@@ -42,15 +53,13 @@ describe('CacheMap.rememberAsync', () => {
     expect(Object.keys(cache.get('metadata'))).toContain('date')
   })
 
-  test('async remember with sync handler', async () => {
+  test('caches the result of a sync handler', async () => {
     await cache.rememberAsync('eyes', () => ({ color: 'yellow' }))
-
     expect(Object.keys(cache.get('eyes'))).toContain('color')
   })
 
-  test('async remember with direct value', async () => {
+  test('caches a primitive value', async () => {
     await cache.rememberAsync('planet', 'Earth')
-
     expect(cache.get('planet')).toBe('Earth')
   })
 })
