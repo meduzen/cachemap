bf872f86eca01a77d93dd21fc2360689cf9d129c
Mehdi M <hi@mehdi.cc>
2023-09-30 20:57:17 +0200

    Move tests not dealing with expiration to their own file

--- tests/cachemap.test.js
+++ tests/cachemap.test.js
@@ -6,56 +6,6 @@ let cache
 
 beforeEach(() => cache = new CacheMap())
 
-describe('CacheMap.add', () => {
-  test('returns its CacheMap instance', () => expect(cache.add('key', 0)).toBe(cache))
-
-  test('caches a primitive value', () => {
-    cache.add('key', 1)
-    cache.add('key', 2)
-
-    expect(cache.get('key')).toBe(1)
-  })
-})
-
-describe('CacheMap.remember', () => {
-  test('caches the result of a sync handler', () => {
-    cache.remember('memories', () => 7 + 14)
-    cache.remember('memories', () => 3 + 2)
-
-    expect(cache.get('memories')).toBe(21)
-  })
-
-  test('caches a primitive value', () => {
-    cache.remember('memories primitive', 4)
-    cache.remember('memories primitive', 6)
-
-    expect(cache.get('memories primitive')).toBe(4)
-  })
-})
-
-describe('CacheMap.rememberAsync', () => {
-  test('caches the result of an async handler', async () => {
-
-    // function emulating an async network call
-    const fetchMetadata = async () => ({ date: new Date(), things: ['thing1'] })
-
-    await cache.rememberAsync('metadata', fetchMetadata)
-    await cache.rememberAsync('metadata', () => ({ color: 'yellow' }))
-
-    expect(Object.keys(cache.get('metadata'))).toContain('date')
-  })
-
-  test('caches the result of a sync handler', async () => {
-    await cache.rememberAsync('eyes', () => ({ color: 'yellow' }))
-    expect(Object.keys(cache.get('eyes'))).toContain('color')
-  })
-
-  test('caches a primitive value', async () => {
-    await cache.rememberAsync('planet', 'Earth')
-    expect(cache.get('planet')).toBe('Earth')
-  })
-})
-
 describe('CacheMap.remember', () => {
   test('sets expiration using remember (with a direct value) with a duration', async () => {
     const EXPIRE_AFTER_MS = 10
@@ -124,8 +74,6 @@ describe('CacheMap.remember', () => {
 })
 
 describe('CacheMap.add', () => {
-  test('returns its CacheMap instance', () => expect(cache.add('key', 0, 1)).toBe(cache))
-
   test('sets expiration using add with a duration', async () => {
     const EXPIRE_AFTER_MS = 10
 
